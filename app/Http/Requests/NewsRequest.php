9f81939b802c4b9449d92ea7666223f5d082e797
IPKIN9 <irwandirini@gmail.con>
2021-09-17 22:55:54 +0800

    Remove comment

--- app/Http/Requests/NewsRequest.php
+++ app/Http/Requests/NewsRequest.php
@@ -6,15 +6,11 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class NewsRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     *
-     * @return bool
-     */
     public function authorize()
     {
         return true;
     }
+
     public function rules()
     {
         return [
